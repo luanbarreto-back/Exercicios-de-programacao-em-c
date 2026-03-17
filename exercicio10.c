135ee69b6adf47a24eddcd576992e87f9f7ef4e9
Luan Barreto <luanbarreto5278@gmail.com>
2026-03-17 09:17:22 -0300

    Add integer division program in exercicio10.c

---
+++ exercicio10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Digite dois numeros inteiros: ");
+    scanf("%d %d", &a, &b);
+    printf("Quociente da divisao inteira: %d\n", a / b);
+    return 0;
+}
