ec539f02ffc635b2b0aeb948f0c42f494ebeff3e
Luan Barreto <luanbarreto5278@gmail.com>
2026-03-17 09:14:29 -0300

    Add program to calculate square root of a number

---
+++ exercicio6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double n;
+    printf("Digite um numero: ");
+    scanf("%lf", &n);
+    printf("Raiz quadrada: %.2lf\n", sqrt(n));
+    return 0;
+}
