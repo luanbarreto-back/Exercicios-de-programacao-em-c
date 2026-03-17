b74899ce4b05a47457a7cd262199a5178cf1416c
Luan Barreto <luanbarreto5278@gmail.com>
2026-03-17 09:12:48 -0300

    Add multiplication output to exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -4,6 +4,6 @@ int main() {
     int a, b;
     printf("Digite dois numeros: ");
     scanf("%d %d", &a, &b);
-    printf("Soma: %d\n", a + b);
+    printf("multiplicacao: %d\n", a * b);
     return 0;
 }
