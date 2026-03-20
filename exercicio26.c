2e9c58effd3b0540fa67b76398d10894501194cc
Luan Barreto <luanbarreto5278@gmail.com>
2026-03-19 22:01:01 -0300

    Implement sequence printing function
    Add function to print a sequence of numbers.

---
+++ exercicio26.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Função que imprime a sequência
+void imprimirSequencia(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+}
